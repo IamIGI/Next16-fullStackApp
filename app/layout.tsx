0b56ff27cad9744286e6acee706f6076d965c898
Igor Klusek <igorklusek@monogo-001s-macbook-pro-2.home>
2026-02-07 16:34:36 +0100

    N16-01-Adding components

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import {
 } from 'next/font/google';
 import './globals.css';
 import LightRays from '../components/LightRays';
+import Navbar from '@/components/Navbar';
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: '--font-geist-sans',
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body
         className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
       >
+        <Navbar />
         <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
           <LightRays
             raysOrigin="top-center-offset"
